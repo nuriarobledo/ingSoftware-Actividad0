994d99607107f61b743b2a3befbfb785e53762ed
nuriarobledo <nuriarobledo0@gmail.com>
2024-04-13 19:43:35 -0300

    [Parte 4.1.3] Modifique la funcion results y agregue su template

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     # ex: /polls/specifics/5/
     path("specifics/<int:question_id>/", views.detail, name="detail"),
     path("specifics/<int:question_id>/results/", views.results, name="results"),
-    path("specifics/<int:question_id>/vote/", views.vote, name="vote")]
+    path("specifics/<int:question_id>/vote/", views.vote, name="vote")
+    ]
